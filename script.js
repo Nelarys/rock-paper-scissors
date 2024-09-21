c70d62a03f1c50bff83d1953d1f1253b73ebc121
Nelarys <77840649+Nelarys@users.noreply.github.com>
2024-09-21 21:08:59 +0200

    feat: Add function getHumanChoice.
    This function will prompt the user to enter a choice and return it.
    It will keep prompting untill a valid choice is entered.

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
 }
+
+function getHumanChoice() {
+  let choice;
+  while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+    choice = prompt(
+      "Enter your choice: rock, paper, or scissors",
+      "Rock",
+    ).toLowerCase();
+  }
+  return choice;
+}
