d3627fb822454797764f3ed71b3ca4766c6df7da
Nelarys <77840649+Nelarys@users.noreply.github.com>
2024-09-21 21:27:12 +0200

    feat: Add function playRound.
    This function plays a round of rock-paper-scissors.
    it checks who won and changes the scores acordingly.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
@@ -6,10 +9,26 @@ function getComputerChoice() {
 function getHumanChoice() {
   let choice;
   while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-    choice = prompt(
-      "Enter your choice: rock, paper, or scissors",
-      "Rock",
-    ).toLowerCase();
+    choice = prompt("Enter your choice: rock, paper, or scissors", "Rock");
+    if (choice === null) continue;
+    choice = choice.toLowerCase();
   }
   return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (humanChoice == "rock" && computerChoice === "scissors") ||
+    (humanChoice == "paper" && computerChoice === "rock") ||
+    (humanChoice == "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  }
+  console.log(`Human: ${humanScore} Computer: ${computerScore}`);
 }
